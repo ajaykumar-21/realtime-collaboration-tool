bf3a60626c166171289e68880659dd516e276fe5
ajaykumar-21 <ajaykumarpk789@gmail.com>
2024-12-03 02:07:01 +0530

    remove / from websocket url

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:3000", // Your development frontend
-      "https://realtime-collaboration-tool.vercel.app/", // Your Vercel production frontend
+      "https://realtime-collaboration-tool.vercel.app", // Your Vercel production frontend
     ],
     methods: ["GET", "POST"], // HTTP methods allowed
   },
