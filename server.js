5e68adecb3e4f6b81846ef0a9e71c7896aa7f117
ajaykumar-21 <ajaykumarpk789@gmail.com>
2025-05-03 14:12:41 +0530

    add credentials for server

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ const io = new Server(server, {
       "https://realtime-collaboration-tool.vercel.app", // Production frontend
     ],
     methods: ["GET", "POST"],
+    credentials: true,
   },
 });
 
